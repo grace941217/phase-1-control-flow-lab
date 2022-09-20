fb78e350bf2771d5856a80d270c347546cecfdd2
Grace <grace941217@gmail.com>
2022-09-20 19:20:03 -0400

    commit tree bacf5f7bce0d2178e56c8515ac4f87dc233f42fb

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+      return 'This one is on me!'
+  } else if (distance >= 400 && distance <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if (distance >= 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else (distance > 2500)
+  return "No can do."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(199);
+
+scuberGreetingForFeet(1500);
+
+scuberGreetingForFeet(2001);
+
+scuberGreetingForFeet(2501);
+
+
+
+function ternaryCheckCity(city) {
+  return (city === 'NYC'? 'Ok, sounds good.' : 'No go.');
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log(ternaryCheckCity('NYC'));
+
+console.log(ternaryCheckCity('Pittsburgh'));
+
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
+
+(switchOnCharmFromTip('generous'));
+
